0a9f11d70bf7b558676731e2b5ec73fce69ec893
mr.code 7 <you@example.com>
2022-06-26 17:42:57 +0300

    Update files

--- src/App.js
+++ src/App.js
@@ -6,15 +6,14 @@ function App() {
     <div className="card">
       <div className="img">
         <img src={img} alt="" />
-
       </div>
+
       <div className="body">
 
         <h1 className="header">Get <span>insights</span> that help your business grow.</h1>
         <p className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aliquam minus nobis placeat distinctio perferendis, soluta eveniet non, </p>
 
         <div className="detailes">
-
           <div className="nums">
             10k+ 
             <div>companies</div>
@@ -23,16 +22,13 @@ function App() {
             314 
             <div>tamplates</div>
           </div>
-          
           <div className="nums">
             12M+ 
             <div>queries</div>
           </div>
-          
-
         </div>
-      </div>
 
+      </div>
     </div>
   );
 }
